acc2e0af941bd26bb03e8303785defe862e65cf4
Jon Osterman <suni.abraham@gmail.com>
2018-01-31 20:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function nowServing(){
     return(`There is nobody waiting to be served!`)
 
   } else {
-    return (`Currently serving ${katzDeli(0)}.`;
+    return (`Currently serving ${katzDeli(0)}.`);
     katzDeli.shift();}
 }
