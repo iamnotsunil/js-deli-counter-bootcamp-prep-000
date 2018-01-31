d833761a5655175fe10c90474c1fe4fd87832e22
Jon Osterman <suni.abraham@gmail.com>
2018-01-31 20:25:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function takeANumber(katzDeli,newbie){
   katzDeli.push(`${newbie}`);
   return(`Welcome, ${newbie}. You are number ${katzDeli.length} in line.`);
 }
+
+function nowServing(){
+  if (katzdeli.length > 0) {
+    return katzDeli(0);
+    katzDeli.shift();
+  } else {
+    return(`There is nobody waiting to be served!`)
