96cfb7697dad8e5cad94e8c77f153b06c70c051d
Jon Osterman <suni.abraham@gmail.com>
2018-01-31 21:01:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeli){
 }
 
 function currentLine(katzDeli){
-var lineup [];
+var lineup = [];
   for (let i=0; i<katzDeli.length; i++) {
     lineup.push(` ` + [i+1]+ `. ` + katzDeli[i])
   }
